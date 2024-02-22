4ab68fe08d36fac31b0f666fd635bcfaea5ac89d
fanghaoyu <address>
2024-02-22 21:27:51 +0800

    大文件分段上传

--- routes/index.js
+++ routes/index.js
@@ -76,4 +76,81 @@ router.post('/upload',function(req,res){
   })
 })
 
+// 提取文件名后缀
+const extractExt = filename => {
+  return filename.slice(filename.lastIndexOf('.'), filename.length)
+}
+router.post('/merge',async(req,res)=>{
+  const { fileHash, fileName, size } = req.body
+  // 如果存在该文件不需要再合并
+  const filePath = path.resolve(UPLOAD_DIR, fileHash + extractExt(fileName))
+  if (fse.existsSync(filePath)) {
+    res.status(200).json({
+      ok: true,
+      msg:"文件已经存在，合并成功"
+    })
+    return
+  }
+  // 如果不存在就合并
+  const chunkDir = path.resolve(UPLOAD_DIR, fileHash)
+  if (!fse.existsSync(chunkDir)) {
+    res.status(401).json({
+      ok: false,
+      msg:"文件不存在，合并失败"
+    })
+    return
+  }
+  // 合并操作
+  const chunkPaths = await fse.readdir(chunkDir)
+  chunkPaths.sort((a, b) => {
+    return a.split('-')[1] - b.split('-')[1]
+  })
+  const list = chunkPaths.map((chunkName, index) => {
+    return new Promise(resolve => {
+      const chunkPath = path.resolve(chunkDir, chunkName)
+      const readStream = fse.createReadStream(chunkPath)
+      const writeStream = fse.createWriteStream(filePath, {
+        start: index * size,
+        end: (index + 1) * size
+      })
+      readStream.on('end', async () => {
+        await fse.unlinkSync(chunkPath)
+        resolve()
+      })
+      readStream.pipe(writeStream)
+    })
+  })
+  await Promise.all(list)
+  fse.remove(chunkDir)
+  
+  res.status(200).json({
+    ok: true,
+    msg:"文件合并成功"
+  })
+})
+
+router.post("/verify",async (req, res) => {
+  const { fileHash, fileName } = req.body
+  const filePath = path.resolve(UPLOAD_DIR, fileHash + extractExt(fileName))
+  // 返回服务器已经上传成功的切片
+  const chunkDir = path.resolve(UPLOAD_DIR, fileHash)
+  let chunkPaths = []
+  if (fse.existsSync(chunkDir)) {
+    chunkPaths = await fse.readdir(chunkDir)
+  }
+  // 如果存在就不需要上传
+  if (fse.existsSync(filePath)){
+    res.status(200).json({
+      ok: true,
+      shouldUpload: false
+    })
+  } else {
+    res.status(200).json({
+      ok: true,
+      shouldUpload: true,
+      existChunks:chunkPaths
+    })
+    
+  }
+})
 module.exports = router;
